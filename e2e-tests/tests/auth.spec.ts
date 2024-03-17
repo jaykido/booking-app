cf08fc96fb76af819abe4f1f40f25ceefd00625d
jaykido <jaykidokenya@gmail.com>
2024-03-17 20:18:05 +0300

    end to end testing using Playwright

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -6,12 +6,12 @@ test("should allow the user to sign in", async ({ page }) => {
 
   // Get the sign in button
 
-  await page.getByRole("link", { name: "Sign In" }).click();
+  await page.getByRole("link", { name: "Sign-In" }).click();
 
-  await expect(page.getByRole("heading", { name: "SIgn In" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email").fill("mercymbu@gmail.com");
-  await page.locator("[name=password").fill("123457");
+  await page.locator("[name=email]").fill("mercymbu@gmail.com");
+  await page.locator("[name=password]").fill("123456");
 
   await page.getByRole("button", { name: "Login" }).click();
 
@@ -20,15 +20,3 @@ test("should allow the user to sign in", async ({ page }) => {
   await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
-
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
-
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
